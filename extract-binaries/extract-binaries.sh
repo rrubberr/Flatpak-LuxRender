aa6fd54065e8721610a859d4ea70640887d3f301
rrubberr <red2x4@gmail.com>
2023-09-11 15:57:34 -0700

    Added OIIO Dependencies Build

--- extract-binaries/extract-binaries.sh
+++ extract-binaries/extract-binaries.sh
@@ -15,7 +15,6 @@ cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/bin
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/bin/benchsimple .
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libpython3.so .
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libpython3.5m.so.1.0 .
-cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libOpenImageIO.so.1.6.3 ./libOpenImageIO.so.1.6
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libboost_thread.so.1.56.0 .
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libboost_program_options.so.1.56.0 .
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libboost_filesystem.so.1.56.0 .
@@ -28,10 +27,14 @@ cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libboost_atomic.so.1.56.0 .
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libboost_python3.so.1.56.0 .
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libboost_python3.so.1.56.0 ./libboost_python.so.1.56.0
-cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libIlmImf-2_2.so.22.0.0 ./libIlmImf-2_2.so.22
-cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libHalf.so.12.0.0 ./libHalf.so.12
-cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libIex-2_2.so.12.0.0 ./libIex-2_2.so.12
-cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libIlmThread-2_2.so.12.0.0 ./libIlmThread-2_2.so.12
+cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libOpenImageIO_Util.so.2.6.0 .
+cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libOpenImageIO.so.2.6.0 .
+cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libImath-3_2.so.30 .
+cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libOpenEXR-3_2.so.31 .
+cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libOpenEXRCore-3_2.so.31 .
+cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libIlmThread-3_2.so.31 .
+cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libIex-3_2.so.31 .
+cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libdeflate.so.0 .
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libembree.so.2.17.7 ./libembree.so.2
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libtbb.so ./libtbb.so
 cp ~/.local/share/flatpak/app/org.luxrender.luxrender17/current/active/files/lib/libtbbmalloc.so ./libtbbmalloc.so
